3faa931806f4932b3a83fc6f284b001da09ff355
Romain Butteaud <romain.butteaud@gmail.com>
2020-12-28 18:13:33 +0100

    fix: add error code if buyTokenPercentageFee and AffiliateFeeType.PositiveSlippageFee are used at the same time

--- src/handlers/swap_handlers.ts
+++ src/handlers/swap_handlers.ts
@@ -348,7 +348,7 @@ const parseGetSwapQuoteRequestParams = (
     const feeType =
         req.query.feeType === undefined
             ? AffiliateFeeType.PercentageFee
-            : req.query.feeType === 'slippage'
+            : req.query.feeType === 'positive-slippage'
             ? AffiliateFeeType.PositiveSlippageFee
             : AffiliateFeeType.PercentageFee;
     const feeRecipient = req.query.feeRecipient as string;
@@ -372,6 +372,23 @@ const parseGetSwapQuoteRequestParams = (
             },
         ]);
     }
+
+    // can't have percentage fee and positive slippage fee at the same time
+    if (buyTokenPercentageFee && feeType === AffiliateFeeType.PositiveSlippageFee) {
+        throw new ValidationError([
+            {
+                field: 'buyTokenPercentageFee',
+                code: ValidationErrorCodes.UnsupportedOption,
+                reason: ValidationErrorReasons.MultipleFeeTypesUsed,
+            },
+            {
+                field: 'feeType',
+                code: ValidationErrorCodes.UnsupportedOption,
+                reason: ValidationErrorReasons.MultipleFeeTypesUsed,
+            },
+        ]);
+    }
+
     const affiliateFee = feeRecipient
         ? {
               feeType,
